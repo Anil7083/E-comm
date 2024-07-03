211ec162052b553ec21ce8f4f11603826fefb812
Anil7083 <wwwanilabnave2@gmail.com>
2024-07-03 11:54:04 +0530

    Update login.js

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -12,7 +12,7 @@ const Login =()=>{
     })
     const handleLogin= async()=>{
         console.warn("email ,password",email,password)
-        let result=await fetch('https://e-comm-backend-c3d8.onrender.com/login',{
+        let result=await fetch('https://e-comm-backend-lovat.vercel.app/login',{
             method:'post',
             body:JSON.stringify({email,password}),
             headers:{
